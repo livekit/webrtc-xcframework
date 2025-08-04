99871e60620bbaba0d0f2208d46890573df9604f
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-08-04 08:02:50 +0100

    137.7151.02 (#8)
    Updates visionOS deployment target, Xcode version and README.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
-// swift-tools-version:5.7
-// (Xcode14.0+)
+// swift-tools-version:5.9
+// (Xcode15.0+)
 
 import PackageDescription
 
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LiveKitWebRTC",
-            url: "https://github.com/livekit/webrtc-xcframework/releases/download/125.6422.33/LiveKitWebRTC.xcframework.zip",
-            checksum: "42085628276ad8ca5b14e2e04f952253863d4df8bd3a6fa3e9a6b5fb44543bbf"
+            url: "https://github.com/livekit/webrtc-xcframework/releases/download/137.7151.02/LiveKitWebRTC.xcframework.zip",
+            checksum: "af120b463cf1e286f43382076faad53019aea16d4b76efd1c71939d6b3f3ebeb"
         ),
     ]
 )
