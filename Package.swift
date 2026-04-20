37202c5fe4f7de8f38bc33c647fcf750121c200f
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2026-04-20 13:54:21 +0800

    144.7559.04

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "LiveKitWebRTC",
-            url: "https://github.com/livekit/webrtc-xcframework/releases/download/144.7559.03/LiveKitWebRTC.xcframework.zip",
-            checksum: "892898ba14293182d6821da68b413a60cff8157d65f380b7a423fdba07b2d4fd"
+            url: "https://github.com/livekit/webrtc-xcframework/releases/download/144.7559.04/LiveKitWebRTC.xcframework.zip",
+            checksum: "46ac123e5c50491d809efc124227839a10ef4c69a63afc47f417cffeeac2540f"
         ),
     ]
 )
